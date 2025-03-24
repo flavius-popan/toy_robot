6df3e09eb92552662bdebc0d8453ddcb10c8c149
Flavius Popan <flaviuspopan@gmail.com>
2025-03-24 15:50:28 -0400

    east/west & doctests done

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -1,18 +1,33 @@
-defmodule ToyRobot do
-  @moduledoc """
-  Documentation for `ToyRobot`.
+defmodule ToyRobot.Robot do
+  @doc """
+  Moves the robot east one space.
+
+  iex> alias ToyRobot.Robot
+  ToyRobot.Robot
+  iex> robot = %{position: 0}
+  %{position: 0}
+  iex> robot |> Robot.move_east
+  %{position: 1}
+  iex> robot |> Robot.move_east |> Robot.move_east |> Robot.move_east
+  %{position: 3}
   """
+  def move_east(robot) do
+    %{position: robot.position + 1}
+  end
 
   @doc """
-  Hello world.
+  Moves the robot west one space.
 
-  ## Examples
-
-      iex> ToyRobot.hello()
-      :world
-
+  iex> alias ToyRobot.Robot
+  ToyRobot.Robot
+  iex> robot = %{position: 0}
+  %{position: 0}
+  iex> robot |> Robot.move_west
+  %{position: -1}
+  iex> robot |> Robot.move_west |> Robot.move_west |> Robot.move_west
+  %{position: -3}
   """
-  def hello do
-    :world
+  def move_west(robot) do
+    %{position: robot.position - 1}
   end
 end
